3032ae6303a8d9804f8d0b88b25c295c9b6c8c38
band96 <bandskt@naver.com>
2022-08-20 18:50:39 +0900

    20-08-14

--- kasta/tsa/Assets/Scripts/Player.cs
+++ kasta/tsa/Assets/Scripts/Player.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    private Transform tr;
+    public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-
+        tr = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float X = Input.GetAxisRaw("Horizontal");
-        if ((isTouchRight && X == 1) || (isTouchLeft && X == -1))
-            X = 0;
+        float h =Input.GetAxis("Horizontal");
+        float v =Input.GetAxis("Vertical");
 
-        float Y = Input.GetAxisRaw("Vertical");
-        if ((isTouchTop && Y == 1) || (isTouchBottom && Y == -1))
-            Y = 0;
-        transform.Translate(new Vector3(X, Y, 0) * Time.deltaTime * Speed);
+        Debug.Log("h = " + h);
+        Debug.Log("v = " + v);
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(moveDir.normalized * moveSpeed*Time.deltaTime);
     }
     
 }
