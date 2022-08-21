803567d902adcdbb53d0f05fd535e7dc41f829da
band96 <bandskt@naver.com>
2022-08-21 19:31:17 +0900

    22-08-21

---
+++ kasta/tsa/Assets/Scripts/FollowAsmera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAsmera : MonoBehaviour
+{
+
+    public Transform targetTr;
+    private Transform camTr;
+
+    [Range(2.0f, 20.0f)]
+    public float distance = 10.0f;
+
+    [Range(2.0f, 20.0f)]
+    public float height = 2.0f;
+
+    public float damping = 10.0f;
+    public float targetOffset = 2.0f;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camTr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        Vector3 pos= transform.position = targetTr.position + (-targetTr.forward * distance)+(Vector3.up* height);
+        camTr.position = Vector3.SmoothDamp(camTr.position, pos, ref velocity, damping);
+        camTr.LookAt(targetTr.position);
+
+    }
+}
--- kasta/tsa/Assets/Scripts/Player.cs
+++ kasta/tsa/Assets/Scripts/Player.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Transform tr;
+    private Animation anim;
+
     public float moveSpeed;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
         tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+
+        anim.Play("Idle");
     }
 
     // Update is called once per frame
@@ -17,6 +23,7 @@ public class Player : MonoBehaviour
     {
         float h =Input.GetAxis("Horizontal");
         float v =Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
         Debug.Log("h = " + h);
         Debug.Log("v = " + v);
@@ -24,6 +31,18 @@ public class Player : MonoBehaviour
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
         tr.Translate(moveDir.normalized * moveSpeed*Time.deltaTime);
+        tr.Rotate(Vector3.up* turnSpeed*Time.deltaTime*r);
+
+        PlayerAnim(h, v);
     }
-    
+
+    void PlayerAnim(float h, float v)
+    {
+        if (v >= 0.1f) anim.CrossFade("RunF", 0.25f);
+        else if (v <= -0.1f) anim.CrossFade("RunB",0.25f);
+        else if (v >= 0.1f) anim.CrossFade("RunR", 0.25f);
+        else if (v <= -0.1f) anim.CrossFade("RunL", 0.25f);
+        else anim.CrossFade("Idle", 0.25f);
+
+    }
 }
