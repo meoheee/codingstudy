194e0ada822445626317b3cd8aab57af6501d096
NamelessSlime <dany1124@hanmail.net>
2022-10-09 19:50:57 +0900

    22

--- daehee/unitystudy/Assets/2.Scripts/RemoveBullet.cs
+++ daehee/unitystudy/Assets/2.Scripts/RemoveBullet.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     private void OnCollisionEnter(Collision coll)
     {
         if (coll.collider.CompareTag("BULLET"))
         {
+            ContactPoint cp = coll.GetContact(0);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+
+            GameObject spark = Instantiate(sparkEffect, cp.point, rot);
+            Destroy(spark, 0.5f);
             Destroy(coll.gameObject);
         }
     }
